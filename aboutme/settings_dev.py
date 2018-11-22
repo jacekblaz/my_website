fd4eeda6c98c23c39f982a1a901040045104a485
jacekblaz <jacekandrzejblazejewski@gmail.com>
2018-11-22 12:28:32 +0100

    to deploy on heroku

--- aboutme/settings_dev.py
+++ aboutme/settings_dev.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/2.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.1/ref/settings/
 """
-
+import django_heroku
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '(-!&^d*fvt#e242^uxok6s!ba&**p#&d6c3v&f3qelwqk=h37p'
+SECRET_KEY = 'lol'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -136,3 +136,4 @@ MEDIA_URL = '/media/'
 LOGIN_REDIRECT_URL = '/'
 LOGIN_URL = 'login/'
 
+django_heroku.settings(locals())
